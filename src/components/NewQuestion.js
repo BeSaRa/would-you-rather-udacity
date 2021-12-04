d0bb1b621e4327b282de0a2442f102e16dd212db
BeSaRa <besara@windowslive.com>
2021-12-05 02:38:24 +0300

    feat: new question component

---
+++ src/components/NewQuestion.js
@@ -0,0 +1,44 @@
+import {useState} from "react";
+import {connect} from "react-redux";
+import {handleAddQuestion} from "../store/actions/questions";
+
+function NewQuestion({authUser, dispatch}) {
+    const [optionOneText, setOptionOne] = useState("");
+    const [optionTwoText, setOptionTwo] = useState("");
+    // optionOneText, optionTwoText, author
+
+    const submitQuestion = () => {
+        dispatch(handleAddQuestion({
+            optionOneText, optionTwoText, author: authUser
+        }))
+    }
+
+    return <div className="d-flex align-items-center justify-content-center">
+        <div className="card shadow-lg col-sm-12 col-md-6">
+            <div className="card-header">
+                <h4 className="text-center">Create New Question</h4>
+            </div>
+            <div className="card-body">
+                <p>Complete the Question</p>
+                <h3>Would You Rather...</h3>
+                <input value={optionOneText} onChange={(e) => setOptionOne(e.target.value)}
+                       placeholder="Enter Option One Text here" className="form-control mb-4"/>
+                <div className="position-relative mb-4">
+                    <hr/>
+                    <h3 className="text-center bg-white position-absolute start-50  top-50 translate-middle">OR</h3>
+                </div>
+                <input value={optionTwoText} onChange={(e) => setOptionTwo(e.target.value)}
+                       placeholder="Enter Option Two Text here" className="form-control mb-4"/>
+                <div className="d-grid">
+                    <button onClick={submitQuestion} className="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+function mapStateToProps({authUser}) {
+    return {authUser}
+}
+
+export default connect(mapStateToProps)(NewQuestion);

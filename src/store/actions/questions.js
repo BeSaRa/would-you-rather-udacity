cac97763935f3889d4a23e593d24d33e18f1ff02
BeSaRa <besara@windowslive.com>
2021-12-05 02:40:52 +0300

    fix: Complete add new Question (Action/Reducer)

--- src/store/actions/questions.js
+++ src/store/actions/questions.js
@@ -1,11 +1,23 @@
+import {_saveQuestion} from "../../API/API";
+
 export const ADD_QUESTION = 'ADD_QUESTION';
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 
-export function receiveQuestionsAction(questions){
+export function receiveQuestionsAction(questions) {
     return {
-        type: RECEIVE_QUESTIONS,
-        payload: questions
+        type: RECEIVE_QUESTIONS, payload: questions
     }
 }
 
+function addQuestion(question) {
+    return {
+        type: ADD_QUESTION, payload: question
+    }
+}
 
+export const handleAddQuestion = (question) => (dispatch) => {
+    _saveQuestion(question)
+        .then((formattedQuestion) => {
+            dispatch(addQuestion(formattedQuestion));
+        })
+}
--- src/store/reducers/questions.js
+++ src/store/reducers/questions.js
@@ -1,9 +1,11 @@
-import {RECEIVE_QUESTIONS} from "../actions/questions";
+import {ADD_QUESTION, RECEIVE_QUESTIONS} from "../actions/questions";
 
 export default function questions(state = {}, action) {
     switch (action.type) {
         case RECEIVE_QUESTIONS:
             return {...action.payload}
+        case ADD_QUESTION:
+            return {...state, [action.payload.id]: action.payload}
         default:
             return state;
     }

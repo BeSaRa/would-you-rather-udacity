9e79597942ba9b08b0e9aac604cc54d0fd87dff1
BeSaRa <besara@windowslive.com>
2021-12-05 02:32:46 +0300

    feat: Page Not Found Component

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,10 @@
+function PageNotFound() {
+    return (
+        <div className="d-flex align-items-center justify-content-center flex-column">
+            <img alt="Page not found" src="/404.gif"/>
+            <h1>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default PageNotFound
